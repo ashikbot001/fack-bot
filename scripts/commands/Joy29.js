0bbd5f0ddd313cc50cb5657475212c761bed5599
ashikbot001 <bossagor745@gmail.com>
2024-10-01 01:43:14 +0600

    Update Joy29.js

--- scripts/commands/Joy29.js
+++ scripts/commands/Joy29.js
@@ -18,7 +18,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   const body = content.toLowerCase();
 	if (body.indexOf(" ")==0 || body.indexOf("😶")==0 || body.indexOf("🫡")==0 || body.indexOf("🤥")==0) {
 		var msg = {
-				body: "কি চাউ তুমি তোমাকে ছেরে অনেক দূরে চলে যাই?😅\n\n𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐄𝐃 𝐉𝐎𝐘",
+				body: "কি চাউ তুমি তোমাকে ছেরে অনেক দূরে চলে যাই?😅\n\nBOSS~ASHIK",
 				attachment: fs.createReadStream(__dirname + `/Joy/joyefa11.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
